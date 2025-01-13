372154e05e4c3583d8e241569323a158ea4cb5fc
RevazPeradze <my.reziko@gmail.com>
2025-01-13 17:00:16 +0400

    second commit

---
+++ lvl33/homework/homework3.py
@@ -0,0 +1,4 @@
+def find_smallest(numbers):
+    return min(numbers)
+
+print(find_smallest([4, 2, 9, 1]))
---
+++ lvl33/homework/homework4.py
@@ -0,0 +1,5 @@
+def is_positive(num):
+    return num > 0
+
+print(is_positive(5))
+print(is_positive(-3))
