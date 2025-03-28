cb40002d8b055b2a8d8535f3f15e8b9ee6f53223
RevazPeradze <my.reziko@gmail.com>
2025-03-28 21:00:58 +0400

    commit

---
+++ lvl43/codewars/codewars.py
@@ -0,0 +1,5 @@
+#1. https://www.codewars.com/kata/523b4ff7adca849afe000035
+#2. https://www.codewars.com/kata/56200d610758762fb0000002
+#3. https://www.codewars.com/kata/55685cd7ad70877c23000102
+#4. https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+#5. https://www.codewars.com/kata/57089707fe2d01529f00024a
