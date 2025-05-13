91a58860fed2dfd7e404ece2a5477dd7cce3cbcc
RevazPeradze <my.reziko@gmail.com>
2025-05-13 23:39:25 +0400

    commit

---
+++ lvl50/homework/script.js
@@ -0,0 +1,11 @@
+let header = document.getElementById('header');
+let paragraph = document.getElementById('paragraph');
+
+header.style.color = 'blue';
+header.style.fontSize = '40px';
+
+paragraph.style.color = 'green';
+paragraph.style.fontSize = '20px';
+
+console.log(header);
+console.log(paragraph);
