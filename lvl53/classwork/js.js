8a9e06bc80163ffe8794f26f69def1a88988cac5
RevazPeradze <my.reziko@gmail.com>
2025-06-08 17:45:29 +0400

    commit

---
+++ lvl53/classwork/js.js
@@ -0,0 +1,4 @@
+let a=1,b="b",c=true,d=[1],e={}
+console.log(a+10,b,c,d,e)
+document.getElementById("h").textContent="t2"
+document.getElementById("p").textContent="new"
---
+++ lvl54/classwork/script.js
@@ -0,0 +1,13 @@
+let black = document.getElementById('icantbreathe').style.color = 'red'
+
+console.log(black)
+
+
+
+let ballz = document.getElementById('steelballz')
+
+console.log(ballz)
+
+let kitty = document.getElementById('meow')
+
+console.log(kitty)
